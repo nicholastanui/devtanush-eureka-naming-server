8b43bf33e83e2ef7462bd8ce9b62531b29b01c8a
ntanui <ntanui@safaricom.co.ke>
2021-04-10 13:30:29 +0300

    init setup

---
+++ src/main/java/com/devtanush/microservices/devtanusheurekanamingserver/devtanusheurekanamingserver/DevtanushEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.devtanusheurekanamingserver.devtanusheurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtanushEurekaNamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtanushEurekaNamingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtanush/microservices/devtanusheurekanamingserver/devtanusheurekanamingserver/DevtanushEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.devtanusheurekanamingserver.devtanusheurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtanushEurekaNamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
